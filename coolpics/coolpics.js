3e65f44abc4cfcdc0b6e047607fc74111e05857c
Michael L <mikemaster98@gmail.com>
2026-01-31 22:37:03 -0700

    Finished Week 4 Prove

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -3,6 +3,11 @@ const modal = document.querySelector('dialog');
 const modalImage = modal.querySelector('img');
 const closeButton = modal.querySelector('.close-viewer');
 
+const menuBtn = document.querySelector('#menu-btn')
+const menu = document.querySelector('nav');
+const mediaQuery = window.matchMedia('(max-width: 1000px)');
+
+
 pictureBox.addEventListener('click', openModal);
 
 function openModal(e) {
@@ -27,3 +32,24 @@ modal.addEventListener('click', (event) => {
     }
 });
 
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    menu.classList.toggle('hide');
+    console.log('clicky');
+}
+
+
+mediaQuery.addEventListener('change', handleScreenChange);
+
+function handleScreenChange(e) {
+    if (e.matches) {
+        // Screen is >= 700px
+        menu.classList.add('hide');
+    }
+    else {
+        menu.classList.remove('hide');
+    }
+}
+
+//Problem: 
