1495ca2360d9c019a2f99fe7fa99682cee178379
Michael L <mikemaster98@gmail.com>
2026-02-28 12:19:59 -0700

    It's beautiful

--- rpg/character.js
+++ rpg/character.js
@@ -1,7 +1,12 @@
 const attackedButton = document.querySelector("#attacked-button");
 const levelUpButton = document.querySelector("#level-up-button");
 
-
+var audio = document.getElementById("babe");
+audio.volume = 0.25;
+var dance = document.getElementById("dance");
+dance.volume = 0.25;
+var cruel = document.getElementById("cruel");
+cruel.volume = 0.25;
 
 const character = {
     title: "Jareth, the Goblin King",
@@ -15,18 +20,33 @@ const character = {
             character.health -= 20;
             console.log(character.health);
             document.querySelector('#health').textContent = "Health: " + character.health;
+            dance.pause();
+            audio.currentTime = 0;
+            audio.play();
         }
         if (character.health <= 0){
-            const testing = confirm("You died!\nPress OK to Dance Magic Dance, or cancel to return to the page.");
-            if (testing){
-                window.location.href = "https://youtu.be/xBv4Ne67QcM?si=uUWh_jsi9hH_uj5l&t=39";
-            }
+            audio.pause();
+            dance.pause();
+            cruel.play();
+            cruel.addEventListener("ended", function handleEnd() {
+                cruel.removeEventListener("ended", handleEnd); // prevent duplicates
+                confirm("You died!");
+            });
+            // confirm("You died!\nPress OK to Dance Magic Dance, or cancel to return to the page.");
         }
     },
     levelUp: function () {
-        character.level += 1;
-        console.log(character.level);
-        document.querySelector('#level').textContent = "Level: " + character.level;
+        if (character.health >= 1) {
+            character.level += 1;
+            console.log(character.level);
+            document.querySelector('#level').textContent = "Level: " + character.level;
+            audio.pause();
+            dance.currentTime = 0;
+            dance.play();
+        }
+        else {
+            return;
+        }
     }
 }
 
@@ -34,6 +54,7 @@ document.querySelector('#level').textContent = "Level: " + character.level;
 
 levelUpButton.addEventListener('click', character.levelUp);
 attackedButton.addEventListener('click', character.takeDamage);
+
 
 document.querySelector('.character-name').textContent = character.title;
 document.querySelector('.character-class').textContent = "Class: " + character.class;
